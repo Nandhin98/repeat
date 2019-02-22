ff1fb140defef0c6cc84bfaaea3f84f50946e462
Nandhin98 <47237438+Nandhin98@users.noreply.github.com>
2019-02-22 14:45:18 +0530

    Create repeat.c

---
+++ repeat.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<coino.h>
+void main()
+{
+ int i,j,k;
+ scanf("%d",&j);
+ int a[20];
+ for(i=0;i<j;i++);
+ {
+  scanf("%d",&a[i]);
+ }
+ for(i=0;i<j;i++)
+ {
+  for(k=i;k<j;k++)
+ }
+ if(a[i]==a[k])
+ printf("%d",a[i]);
+}
+}
